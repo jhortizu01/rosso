b3678065efaf67049164510eb0f13d772743e610
Phil Than <82873669+pthan1@users.noreply.github.com>
2021-11-23 12:10:16 -0700

    Music player plays nature sounds

--- src/components/MusicContainer.js
+++ src/components/MusicContainer.js
@@ -2,14 +2,13 @@ import React, { useState } from 'react'
 import '../styles/MusicContainer.scss'
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/src/styles.scss'
-import song1 from '../mp3/song1.mp3'
-import song2 from '../mp3/song2.mp3'
-import song3 from '../mp3/song3.mp3'
-import song4 from '../mp3/song4.mp3'
-import song5 from '../mp3/song5.mp3'
+import fireSounds from '../mp3/SharKhuls_Aug2019.mp3'
+import leafSounds from '../mp3/Rains and Bird_RMNP_BT.mp3'
+import waterSounds from '../mp3/Drift Ice at Shiretoko 2.mp3'
+import moonSounds from '../mp3/Kasungu_Night.mp3'
 import arrow from '../assets/Arrow_down.png'
 
-const playlist = [song1, song2, song3, song4, song5]
+const playlist = [fireSounds, leafSounds, waterSounds, moonSounds ]
 
 const MusicContainer = () => {
   const [currentSong, setCurrentSong] = useState(playlist[0])
