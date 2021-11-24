757c92c19f6db0d9f69bbfaa165da4f65bc772ec
Phil Than <82873669+pthan1@users.noreply.github.com>
2021-11-23 22:35:30 -0700

    Play themed song on palette change

--- src/components/MusicContainer.js
+++ src/components/MusicContainer.js
@@ -1,19 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import '../styles/MusicContainer.scss'
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/src/styles.scss'
 import fireSounds from '../mp3/SharKhuls_Aug2019.mp3'
-import leafSounds from '../mp3/Rains and Bird_RMNP_BT.mp3'
+import leafSounds from '../mp3/AshiuForest_River_Nov2018.mp3'
 import waterSounds from '../mp3/Drift Ice at Shiretoko 2.mp3'
 import moonSounds from '../mp3/Kasungu_Night.mp3'
 import arrow from '../assets/Arrow_down.png'
+import { useCanvas } from '../context/CanvasContext'
+
 
 const playlist = [fireSounds, leafSounds, waterSounds, moonSounds ]
 const playlistTitles = [
   "Wind In The Reeds (Great Gobi National Park, Mongolia)",
-  "Rain and Birds (Royal Manas National Park, Bhutan)",
+  "Gentle Stream and Autumn (Ashiu Forest, Kyoto, Japan)",
   "Drift Ice (Shiretoko National Park, Hokkaido, Japan)",
-  "Night (Kasungu National Park, Malawi)"
+  "Gentle Night (Kasungu National Park, Malawi)"
 ]
 
 const MusicContainer = () => {
@@ -21,6 +23,27 @@ const MusicContainer = () => {
   const [musicVisibility, setMusicVisibility] = useState("audio hide")
   const [arrowDirection, setArrowDirection] = useState("arrow-direction down")
   const [currentTitle, setCurrentTitle] = useState("Wind In The Reeds (Great Gobi National Park, Mongolia)");
+  const { activePalette } = useCanvas()
+
+  useEffect(() => {
+    playSongWithCorrespondingPalette();
+  }, [activePalette])
+  
+  const playSongWithCorrespondingPalette = () => {
+    if (activePalette.isFireActive === true) {
+      setCurrentSong(playlist[0])
+       setCurrentTitle(playlistTitles[0])
+    } else if (activePalette.isLeafActive === true) {
+      setCurrentSong(playlist[1])
+       setCurrentTitle(playlistTitles[1])
+    } else if (activePalette.isWaveActive === true) {
+      setCurrentSong(playlist[2])
+       setCurrentTitle(playlistTitles[2])
+    } else if (activePalette.isMoonActive === true) {
+      setCurrentSong(playlist[3])
+       setCurrentTitle(playlistTitles[3])
+    }
+  }
 
   const handleClickPrevious = () => {
     if(currentSong === playlist[0]) {
